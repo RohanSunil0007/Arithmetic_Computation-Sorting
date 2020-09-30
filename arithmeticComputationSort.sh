87ffd25e6f6d5ab7f2b6a68ab83bae1d4ae45380
Rohan Sunil <reach.rohan0007@gmail.com>
2020-09-30 20:25:27 +0530

    Added : computed a*b+c

--- arithmeticComputationSort.sh
+++ arithmeticComputationSort.sh
@@ -3,4 +3,5 @@ echo "Welcome to Arithmetic Computation Sorting program"
 read -p "Enter a :" a
 read -p "Enter b :" b
 read -p "Enter c :" c
-result=$(($a+$b*c))
+result=$(($a+$b*$c))
+result1=$(($a*$b+$c))
